574272750c9b7533633fd8bb19b5d783e9208824
Khalid Sidat <khalidsidat00@icloud.com>
2026-01-20 00:36:30 +0000

    Refactor game victory functions and remove old code

---
+++ gamevictory.js
@@ -0,0 +1,10 @@
+
+function End_game_restartGame() {
+  document.getElementById("end_game_restart").addEventListener("click", function() {window.location.href = "SurvivalGame.html";});
+}
+End_game_restartGame();
+
+function End_game_quitGame() {
+  document.getElementById("end_game_quit").addEventListener("click", function() {window.location.href = "index.html";});
+}
+End_game_quitGame();
